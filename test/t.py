885ae62a609dbb3bfc4d0918ece5ccf5eae02b69
Quitto <brunamoreiraquitto@gmail.com>
2025-07-06 16:58:38 -0300

    1.0v Fix Installer in data.json

--- test/t.py
+++
@@ -1,8 +0,0 @@
-import os
-
-install_path = os.path.join(os.getenv("APPDATA"), "CoreOS_Finace", "data") # Appdata Local App
-key_path = os.path.join(install_path, ".env","key.key")
-
-print(os.getenv("APPDATA"))
-print(install_path)
-print(key_path)
